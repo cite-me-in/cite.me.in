13bc3df701c5aa7c170a9565df5add9115bf2e97
Assaf Arkin <assaf@labnotes.org>
2026-03-19 19:48:31 -0700

    chore: update database backup and restore commands in clone script for consistency with Infisical CLI

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 
 # Dump the database to a file
 echo -e "\033[32m  Dumping database to backup.sql …\033[0m"
-pg_dump "$(infisical secrets get --plain POSTGRES_URL_NON_POOLING)" --file prisma/backup.sql --schema public --clean --no-owner --no-privileges
+pg_dump "$(infisical secrets --env prod get --plain POSTGRES_URL_NON_POOLING)" --file prisma/backup.sql --schema public --clean --no-owner --no-privileges
 
 # Restore the database from the file
 echo -e "\033[32m  Restoring database from backup.sql …\033[0m"
