daa3884f67dcbd44f5a0240b9074102429304623
Assaf Arkin <assaf@labnotes.org>
2026-03-27 16:50:37 -0700

    feat: add setup status polling endpoint

---
+++ app/routes/site.$domain_.setup.status.ts
@@ -0,0 +1,14 @@
+import { requireSiteAccess } from "~/lib/auth.server";
+import { getProgress } from "~/lib/setupProgress.server";
+import type { Route } from "./+types/site.$domain_.setup.status";
+
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const { site, user } = await requireSiteAccess({
+    domain: params.domain,
+    request,
+  });
+
+  const offset = Number(new URL(request.url).searchParams.get("offset") ?? "0");
+  const progress = await getProgress(site.id, user.id, offset);
+  return Response.json(progress);
+}
