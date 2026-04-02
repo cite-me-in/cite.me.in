e2202f2ecd8847558f5cb345f559a6356ed9e963
Assaf Arkin <assaf@labnotes.org>
2026-04-01 18:40:38 -0700

    fix: use optional chain in /r route

--- app/routes/r.ts
+++ app/routes/r.ts
@@ -9,7 +9,7 @@ export async function loader({ request }: Route.LoaderArgs) {
   const email = searchParams.get("email");
   const token = searchParams.get("token");
 
-  const dest = url && url.startsWith(envVars.VITE_APP_URL) ? url : "/";
+  const dest = url?.startsWith(envVars.VITE_APP_URL) ? url : "/";
 
   if (email && token && token === generateUnsubscribeToken(email)) {
     await prisma.user.updateMany({
